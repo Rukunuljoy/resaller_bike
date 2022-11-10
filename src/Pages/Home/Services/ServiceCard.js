503a77b7043dc1a0bb95c63ee59e15982a810b3f
Rukunuljoy <rukunuljoypub58@gmail.com>
2022-11-10 23:52:03 +0600

    final project

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import 'react-photo-view/dist/react-photo-view.css';
 import { Link } from "react-router-dom";
 
 const ServiceCard = ({service}) => {
     const {_id,img,name,price,description} = service;
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
+      <div>
+        <PhotoProvider>
+          <PhotoView src={img}>
+          <img src ={img} alt="" className="rounded-3xl w-full h-72"></img></PhotoView>
+        </PhotoProvider>
+      </div>
       <figure>
         {/* <img src={`/service/${img}`} className="h-72 w-96" alt="" /> */}
         <img src={img} className="h-72 w-96" alt=""/>
